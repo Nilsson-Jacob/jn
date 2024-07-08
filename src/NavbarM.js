ff57840fd6949e197c529d41a4c329b85571bc01
nilsson-jacob <jacobnilsson97@gmail.com>
2024-07-08 10:56:41 +0200

    added

--- src/NavbarM.js
+++ src/NavbarM.js
@@ -10,8 +10,8 @@ function NavbarM() {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">CV</Nav.Link>
+            <Nav.Link href="#pricing">Projects</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
