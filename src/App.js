d02b838b7fbc0a98870adbe30ca374114e91ee2b
nilsson-jacob <jacobnilsson97@gmail.com>
2024-07-08 10:42:30 +0200

    new text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import NavbarM from "./NavbarM";
+
 function App() {
   return (
     <div className="App">
+      <NavbarM></NavbarM>
       <p>JN page</p>
     </div>
   );
